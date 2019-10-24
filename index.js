20de82e058a191223e52117e74bb749d7ab106a9
stephenschab <sschab689@gmail.com>
2019-10-24 14:59:16 -0600

    started user input function

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 $(function() {
 
+  $('form').on(submit, event => {
 
+    event.preventDefault();
+
+    let input = $(event.currentTarget()).find('input[name = "shopping-list-entry"]');
+
+    $('.shopping-list').append(`<li><span class="shopping-item">${input}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+
+  });
   
 });
